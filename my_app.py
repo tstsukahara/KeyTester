6c1cafd25490b0f03227f4d1bf2275e130c095fc
tstsukahara <9626666+tstsukahara@users.noreply.github.com>
2025-01-14 22:56:56 +0900

    リファクタリング

--- my_app.py
+++ my_app.py
@@ -1,20 +1,25 @@
+import shutil
+
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import Qt
 import sys
 import json
 import os
-import shutil
 
 from PyQt5.QtWidgets import QHBoxLayout
 
 QWERT_KEYS = "qwert"
+SWITCH_TYPES = ["Linear", "Tactile", "Clicky", "Silent Linear", "Silent Tactile", "Silent Clicky",]
 
 
 class KeyTesterApp(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Key Switch Tester")
-        self.setGeometry(100, 100, 400, 600)
+        self.setGeometry(100, 100, 300, 400)
+
+        # 表示対象のキー
+        self.key = QWERT_KEYS[0]
 
         # キーマッピングの保存ファイル
         self.config_file = "key_map.json"
@@ -23,116 +28,181 @@ class KeyTesterApp(QtWidgets.QWidget):
         self.key_map = self.load_key_map()
 
         # レイアウトの作成
-        self.layout = QtWidgets.QVBoxLayout()
+        self.main_layout = QtWidgets.QVBoxLayout()
 
-        self.info_label = QtWidgets.QLabel("press any key", self)
-        self.info_label.setAlignment(Qt.AlignCenter)
-        self.info_label.setStyleSheet("font-size: 16px;")
-        self.layout.addWidget(self.info_label)
+        # キー
+        self.key_layout = QtWidgets.QHBoxLayout()
+        self.key_label = QtWidgets.QLabel(self)
+        self.key_label.setAlignment(Qt.AlignCenter)
+        self.main_layout.addWidget(self.key_label)
 
+        # メッセージ表示
+        self.message_label = QtWidgets.QLabel("Press any key.")
+        self.message_label.setAlignment(Qt.AlignCenter)
+        self.main_layout.addWidget(self.message_label)
+
+        # 画像
+        self.image_layout = QtWidgets.QVBoxLayout()
         self.image_label = QtWidgets.QLabel(self)
         self.image_label.setAlignment(Qt.AlignCenter)
-        self.layout.addWidget(self.image_label)
+        self.image_layout.addWidget(self.image_label)
+        self.main_layout.addLayout(self.image_layout)
 
-        self.switch_name_label = QtWidgets.QLabel("", self)
-        self.switch_name_label.setAlignment(Qt.AlignCenter)
-        self.switch_name_label.setStyleSheet("font-size: 20px;")
-        self.layout.addWidget(self.switch_name_label)
+        # 名前
+        self.switch_name_layout = QtWidgets.QHBoxLayout()
+        self.switch_name = QtWidgets.QLabel("", self)
+        self.switch_name.setAlignment(Qt.AlignCenter)
+        self.switch_name_layout.addWidget(self.switch_name)
+        self.main_layout.addLayout(self.switch_name_layout)
 
-        self.settings_button = QtWidgets.QPushButton("Settings", self)
-        self.settings_button.clicked.connect(self.open_settings)
-        self.layout.addWidget(self.settings_button)
+        # タイプ
+        self.switch_type_layout = QtWidgets.QHBoxLayout()
+        self.switch_type = QtWidgets.QLabel("", self)
+        self.switch_type.setAlignment(Qt.AlignCenter)
+        self.switch_type_layout.addWidget(self.switch_type)
+        self.main_layout.addLayout(self.switch_type_layout)
 
-        self.setLayout(self.layout)
+        # 押下圧
+        self.force_layout = QtWidgets.QHBoxLayout()
+        self.force = QtWidgets.QLabel("", self)
+        self.force.setAlignment(Qt.AlignCenter)
+        self.force_layout.addWidget(self.force)
+        self.main_layout.addLayout(self.force_layout)
+
+        # 編集ボタン
+        self.edit_button = QtWidgets.QPushButton("Edit", self)
+        self.edit_button.clicked.connect(self.open_settings)
+        self.edit_button.hide()
+        self.main_layout.addWidget(self.edit_button)
+
+        self.setLayout(self.main_layout)
 
     def load_key_map(self):
         if os.path.exists(self.config_file):
             with open(self.config_file, "r") as file:
                 return json.load(file)
         else:
-            # Initialize key map with QWERTY layout
-            return {key: {"image": None, "switch_name": ""} for key in QWERT_KEYS}
-
-    def save_key_map(self):
-        with open(self.config_file, "w") as file:
-            json.dump(self.key_map, file, indent=4)
+            return {key: None for key in QWERT_KEYS}
 
     def keyPressEvent(self, event):
-        key = event.text().lower()
-        if key in self.key_map:
-            key_info = self.key_map[key]
+        self.key = event.text().lower()
+        self.message_label.hide()
+        self.display_info()
+        self.edit_button.show()
+
+    def display_info(self):
+        # キーの表示
+        self.key_label.setText(f"Key: {self.key}")
+
+        if self.key in self.key_map:
+            key_info = self.key_map[self.key]
 
             # 画像の表示
             if key_info["image"]:
                 pixmap = QtGui.QPixmap(key_info["image"])
-                scaled_pixmap = pixmap.scaled(300, 300, Qt.KeepAspectRatio)
+                scaled_pixmap = pixmap.scaled(200, 200, Qt.KeepAspectRatio)
                 self.image_label.setPixmap(scaled_pixmap)
             else:
                 self.image_label.clear()
 
-            # 説明の表示
-            self.switch_name_label.setText(key_info["switch_name"])
+            # 詳細の表示
+            self.switch_name.setText(f'Name: {key_info["switch_name"]}')
+            self.switch_type.setText(f'Type: {key_info["switch_type"]}')
+            self.force.setText(f'Operation Force: {key_info["operation_force"]}')
         else:
+            self.message_label.setText("No information available.")
+            self.message_label.show()
             self.image_label.clear()
-            self.switch_name_label.setText("No information available")
+            self.switch_name.clear()
+            self.switch_type.clear()
 
     def open_settings(self):
         settings_window = QtWidgets.QDialog(self)
         settings_window.setWindowTitle("Settings")
         main_layout = QtWidgets.QVBoxLayout()
 
-        for key in QWERT_KEYS:
-            key_info = self.key_map[key]
+        # キー
+        key_layout = QtWidgets.QHBoxLayout()
+        key_label = QtWidgets.QLabel(f"Key: {self.key}")
+        key_layout.addWidget(key_label)
+        main_layout.addLayout(key_layout)
 
-            key_label_layout = QHBoxLayout()
-            key_label = QtWidgets.QLabel(f"Key: {key}")
-            key_label.setStyleSheet("font-size: 16px;")
-            key_label_layout.addWidget(key_label)
-            main_layout.addLayout(key_label_layout)
+        # 画像
+        image_layout = QtWidgets.QHBoxLayout()
+        image_label = QtWidgets.QLabel("Image: ")
+        image_layout.addWidget(image_label)
 
-            # Switch Name
-            switch_name_layout = QHBoxLayout()
-            switch_name_title = QtWidgets.QLabel("name: ")
-            switch_name_layout.addWidget(switch_name_title)
-            switch_name_entry = QtWidgets.QLineEdit(self)
-            switch_name_entry.setText(key_info["switch_name"])
-            switch_name_layout.addWidget(switch_name_entry)
-            main_layout.addLayout(switch_name_layout)
+        image = QtWidgets.QLabel(os.path.basename(self.key_map[self.key]["image"]))
+        image_layout.addWidget(image)
+        choose_image_button = QtWidgets.QPushButton("Choose Image", self)
+        image_layout.addWidget(choose_image_button)
+        main_layout.addLayout(image_layout)
 
-            # Image
-            image_layout = QHBoxLayout()
-            image_path_title = QtWidgets.QLabel("image: ")
-            image_layout.addWidget(image_path_title)
+        def choose_image(key):
+            file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select Image", "",
+                                                                 "Images (*.png *.jpg *.jpeg)")
+            if file_path:
+                saved_path = os.path.join(self.image_dir, os.path.basename(file_path))
+                shutil.copy(file_path, saved_path)
+                self.key_map[key]["image"] = saved_path
 
-            image_path_label = QtWidgets.QLabel(key_info["image"])
-            image_layout.addWidget(image_path_label)
-            choose_image_button = QtWidgets.QPushButton("Choose Image", self)
-            image_layout.addWidget(choose_image_button)
-            main_layout.addLayout(image_layout)
+        choose_image_button.clicked.connect(lambda _, key=self.key: choose_image(key))
 
-            def choose_image(key=key):
-                file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select Image", "", "Images (*.png *.jpg *.jpeg)")
-                if file_path:
-                    # Save the selected image with a fixed file name based on the key
-                    saved_path = os.path.join(self.image_dir, os.path.basename(file_path))
-                    shutil.copy(file_path, saved_path)
-                    self.key_map[key]["image"] = saved_path
+        # 名前
+        name_layout = QtWidgets.QHBoxLayout()
+        name_label = QtWidgets.QLabel("Name: ")
+        name_layout.addWidget(name_label)
 
-            choose_image_button.clicked.connect(lambda _, key=key: choose_image(key))
+        name_entry = QtWidgets.QLineEdit(self)
+        name_entry.setText(self.key_map[self.key]["switch_name"])
+        name_layout.addWidget(name_entry)
+        main_layout.addLayout(name_layout)
 
-            def update_info(key=key, entry=switch_name_entry):
-                self.key_map[key]["switch_name"] = entry.text()
+        # タイプ
+        type_layout = QtWidgets.QHBoxLayout()
+        type_label = QtWidgets.QLabel("Type: ")
+        type_layout.addWidget(type_label)
 
-            save_button = QtWidgets.QPushButton("Save", self)
-            save_button.clicked.connect(lambda _, key=key, entry=switch_name_entry: update_info(key, entry))
-            main_layout.addWidget(save_button)
+        type_entry = QtWidgets.QComboBox()
+        type_entry.addItems(SWITCH_TYPES)
+        type_entry.setCurrentText(self.key_map[self.key]["switch_type"])
+        type_layout.addWidget(type_entry)
+        main_layout.addLayout(type_layout)
 
-        save_all_button = QtWidgets.QPushButton("Save All", self)
-        save_all_button.clicked.connect(self.save_key_map)
-        main_layout.addWidget(save_all_button)
+        # 押下圧
+        force_layout = QtWidgets.QHBoxLayout()
+        force_label = QtWidgets.QLabel("Operation Force: ")
+        force_layout.addWidget(force_label)
+
+        force_entry = QtWidgets.QLineEdit(self)
+        force_entry.setText(self.key_map[self.key]["operation_force"])
+        force_layout.addWidget(force_entry)
+        main_layout.addLayout(force_layout)
+
+        # Saveボタン
+        save_button = QtWidgets.QPushButton("Save", self)
+        entry_map = {
+            "switch_name": name_entry,
+            "switch_type": type_entry,
+            "operation_force": force_entry
+        }
+        save_button.clicked.connect(lambda: self.update_key_map(entry_map))
+        main_layout.addWidget(save_button)
 
         settings_window.setLayout(main_layout)
         settings_window.exec_()
+
+    def update_key_map(self, entry_map):
+        self.key_map[self.key]["switch_name"] = entry_map["switch_name"].text()
+        self.key_map[self.key]["switch_type"] = entry_map["switch_type"].currentText()
+        self.key_map[self.key]["operation_force"] = entry_map["operation_force"].text()
+        self.save_key_map()
+        self.display_info()
+
+    def save_key_map(self):
+        print(self.key_map[self.key])
+        with open(self.config_file, "w") as file:
+            json.dump(self.key_map, file, indent=4)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
